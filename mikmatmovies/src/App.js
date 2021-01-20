2ca35b6eded81ce36c51cb3310244368ae3b962c
mattisvainikainen <mattis.vainikainen@gmail.com>
2021-01-20 11:22:13 +0100

    Added Preview component

--- mikmatmovies/src/App.js
+++ mikmatmovies/src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 
 //Imported components
 import Nav from "./Components/Nav";
+import Preview from "./Components/Preview";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Preview />
     </div>
   );
 }
---
+++ mikmatmovies/src/Components/Preview.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Preview.css";
+
+function Preview() {
+  return (
+    <div className="preview-containerq">
+      <h3>Preview</h3>
+    </div>
+  );
+}
+
+export default Preview;
