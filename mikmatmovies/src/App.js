8f40a2e53466613a8a79182077a6ab9a51521aed
mattisvainikainen <mattis.vainikainen@gmail.com>
2021-01-20 09:51:53 +0100

    Created Nav

--- mikmatmovies/src/App.js
+++ mikmatmovies/src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+//Imported components
+import Nav from "./Components/Nav"
+
+
 function App() {
   return (
     <div className="App">
---
+++ mikmatmovies/src/Components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Nav() {
+    return (
+      <nav> 
+          <ul className="nav-ul">
+          </ul>
+      </nav>
+    );
+}
+
+
+
+
+
+export default Nav;
