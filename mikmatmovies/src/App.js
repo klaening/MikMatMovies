476702c0232889ea64ca85c741e5f0f512bc6529
mattisvainikainen <mattis.vainikainen@gmail.com>
2021-01-22 16:24:11 +0100

    merge main

--- mikmatmovies/src/App.js
+++ mikmatmovies/src/App.js
@@ -3,7 +3,6 @@ import "./Components/Global.css";
 
 //Imported components
 import Nav from "./Components/Nav";
-<<<<<<< HEAD
 import Preview from "./Components/Preview";
 
 function App() {
@@ -11,18 +10,6 @@ function App() {
     <div className="App">
       <Nav />
       <Preview />
-=======
-import Card from "./Components/Card";
-
-function App() {
-  return (
-    <div className="main">
-      <div className="App">
-        <Nav />
-        <Card />
-        <Card />
-      </div>
->>>>>>> main
     </div>
   );
 }
