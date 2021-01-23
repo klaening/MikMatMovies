fb2cf001b15b429fec823487b42eaea410540222
mattisvainikainen <mattis.vainikainen@gmail.com>
2021-01-23 17:10:42 +0100

    Added some properties to Video URL

--- mikmatmovies/src/Components/VideoSection.js
+++ mikmatmovies/src/Components/VideoSection.js
@@ -8,44 +8,46 @@ import "./Global.css";
 export class VideoSection extends Component {
   render() {
     return (
-      <Carousel
-        interval={25000}
-        autoPlay={true}
-        prevIcon={false}
-        keyboard={false}
-        pauseOnHover={true}
-      >
-        {VideoData.map((video) => (
-          <Carousel.Item id="item-data" key={video.id}>
-            <div className="item-container">
-              <div className="desc-container">
-                <div className="text-box">
-                  <h3>{video.title}</h3>
-                  <h6>{video.description}</h6>
-                  <h6>Realease Year: {video.RealeaseYear}</h6>
-                  <a href={video.VideoURL}>Watch whole trailer here</a>
+      <div className="main-container">
+        <Carousel
+          interval={25000}
+          autoPlay={true}
+          prevIcon={false}
+          keyboard={false}
+          pauseOnHover={true}
+        >
+          {VideoData.map((video) => (
+            <Carousel.Item id="item-data" key={video.id}>
+              <div className="item-container">
+                <div className="desc-container">
+                  <div className="text-box">
+                    <h3>{video.title}</h3>
+                    <h6>{video.description}</h6>
+                    <h6>Realease Year: {video.RealeaseYear}</h6>
+                    <a href={video.VideoURL}>Watch whole trailer here</a>
+                  </div>
+                  <div className="Image-box">
+                    <img src={video.ImageURL} alt="" />
+                  </div>
                 </div>
-                <div className="Image-box">
-                  <img src={video.ImageURL} alt="" />
+                <div className="video-container">
+                  <iframe
+                    width="100%"
+                    height="100%"
+                    src={video.VideoURL}
+                    frameBorder="0"
+                    gesture="media"
+                    allow="autoplay; encrypted-media"
+                  ></iframe>
+                  <Carousel.Caption>
+                    {/* <h3>RATING: {video.rating}</h3> */}
+                  </Carousel.Caption>
                 </div>
               </div>
-              <div className="video-container">
-                <iframe
-                  width="100%"
-                  height="100%"
-                  src={video.VideoURL}
-                  frameBorder="0"
-                  gesture="media"
-                  allow="autoplay; encrypted-media"
-                ></iframe>
-                <Carousel.Caption>
-                  {/* <h3>RATING: {video.rating}</h3> */}
-                </Carousel.Caption>
-              </div>
-            </div>
-          </Carousel.Item>
-        ))}
-      </Carousel>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      </div>
     );
   }
 }
