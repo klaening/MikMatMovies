88be60dcf235c3913ba09ede16daef62e63d648e
Mikael <m.klaening@gmail.com>
2021-01-29 10:29:24 +0100

    Cards are in a Grid

--- mikmatmovies/src/Components/Card/CardHolder.js
+++ mikmatmovies/src/Components/Card/CardHolder.js
@@ -1,24 +1,49 @@
 import React from "react";
 import Card from "./Card";
 import movies from "./movies.json";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
 import "./CardHolder.css";
 import "../Global.css";
 
-function CardHolder() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    padding: theme.spacing(2),
+  },
+}));
+
+export default function CardHolder() {
+  const classes = useStyles();
+
   return (
     <div className="cardHolder-body">
-      <header>
+      <div className="ch-header">
         <h2>Recommended</h2>
-      </header>
-      <section id="test">
-        {movies.map((movie) => (
-          <div key={movie.id.toString()} className="ch-body">
-            <Card movie={movie} />
-          </div>
-        ))}
-      </section>
+      </div>
+      <div className={classes.root}>
+        <Grid
+          container
+          spacing={2}
+          direction="row"
+          justify="center"
+          alignItems="flex-start"
+        >
+          {movies.map((movie) => (
+            <Grid
+              justify="center"
+              item
+              xs={12}
+              sm={6}
+              md={3}
+              key={movie.id.toString()}
+            >
+              <Card movie={movie} />
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+      <div className="test"></div>
     </div>
   );
 }
-
-export default CardHolder;
