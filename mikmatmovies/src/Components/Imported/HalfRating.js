83505048e706b17870b1c699e76d9fb6d6bc00ce
Mikael <m.klaening@gmail.com>
2021-02-02 11:55:43 +0100

    Fixed bug in Rating

--- mikmatmovies/src/Components/Imported/HalfRating.js
+++ mikmatmovies/src/Components/Imported/HalfRating.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Rating from "@material-ui/lab/Rating";
 import { makeStyles } from "@material-ui/core/styles";
+import "./HalfRating.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,7 +18,12 @@ export default function HalfRating() {
 
   return (
     <div className={classes.root}>
-      <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+      <Rating
+        className="halfRating"
+        name="half-rating"
+        defaultValue={2.5}
+        precision={0.5}
+      />
       {/* <Rating
         name="half-rating-read"
         defaultValue={2.5}
