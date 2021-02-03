641fefcea9acfcc6550532cae99fb926b3b917f1
Mikael <m.klaening@gmail.com>
2021-02-03 17:30:54 +0100

    Basic sketch of what I want it to do

--- mikmatmovies/src/Components/Card/CardHolder.js
+++ mikmatmovies/src/Components/Card/CardHolder.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
 import "./CardHolder.css";
 import "../Global.css";
 
 function CardHolder({ header, movies }) {
+  const [displayAmount, setDisplayAmount] = useState(10);
+
+  const moreMovies = () => {
+    console.log("Before click", displayAmount);
+
+    if (displayAmount < 20) {
+      setDisplayAmount(displayAmount + 10);
+    }
+
+    console.log("After click", displayAmount);
+  };
+
+  const ShowTheCards = () => {
+    return (
+      <div className="cards">
+        {movies.slice(0, 10).map((movie) => (
+          <Card key={movie.id} movie={movie} movies={movies} />
+        ))}
+      </div>
+    );
+  };
+
   return (
     <div>
       {movies.length !== 0 ? (
@@ -13,10 +35,16 @@ function CardHolder({ header, movies }) {
             {/* {subtitle ? <p>{`Results: ${movies.length}`}</p> : null} */}
             <p>{`Results: ${movies.length}`}</p>
           </div>
-          <div className="cards2">
-            {movies.map((movie) => (
+
+          {ShowTheCards()}
+
+          {/* <div className="cards2">
+            {movies.slice(0, 10).map((movie) => (
               <Card key={movie.id} movie={movie} movies={movies} />
             ))}
+          </div> */}
+          <div className="seeMore">
+            <p onClick={moreMovies()}>See more</p>
           </div>
         </div>
       ) : null}
