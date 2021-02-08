d906659eefa05e293d466a4f03c28e6907168066
Mikael <m.klaening@gmail.com>
2021-02-08 21:20:57 +0100

    Merge fix

--- mikmatmovies/src/Components/Footer/Footer.js
+++ mikmatmovies/src/Components/Footer/Footer.js
@@ -6,11 +6,7 @@ const Footer = () => {
   return (
     <footer className="footer_main">
       <ul>
-<<<<<<< HEAD
         <Link to="/about">
-=======
-        <Link to="#">
->>>>>>> b33bcd808a9b71bd8eeea16f1ba6f90dcee7ee9b
           <li>About</li>
         </Link>
         <Link to="#">
