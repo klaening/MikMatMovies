528e227a69828c77a98425253aa76049ee3519cb
Mikael <m.klaening@gmail.com>
2021-02-09 15:42:53 +0100

    About Page with Map and Lorem

--- mikmatmovies/src/Components/Pages/About/About.js
+++ mikmatmovies/src/Components/Pages/About/About.js
@@ -26,18 +26,43 @@ const About = () => {
         qui non, nisi modi eaque quaerat voluptatibus! Odio minus deserunt
         ullam, numquam corrupti illum deleniti!
       </p>
-      <iframe
-        title="map"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8466.599685621235!2d11.834047945440922!3d57.959886849211195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464565ad81c00563%3A0x7d53b1e2ad79254f!2s442%2096%20Sk%C3%A4ggstorp!5e0!3m2!1ssv!2sse!4v1612862379736!5m2!1ssv!2sse"
-        width="600"
-        height="450"
-        frameBorder="0"
-        allowFullScreen=""
-        aria-hidden="false"
-        tabIndex="0"
-      ></iframe>
 
-      <BackToTop />
+      <div className="address">
+        <h2>Address</h2>
+        <div className="map_holder">
+          <div className="map">
+            <iframe
+              title="map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8466.599685621235!2d11.834047945440922!3d57.959886849211195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464565ad81c00563%3A0x7d53b1e2ad79254f!2s442%2096%20Sk%C3%A4ggstorp!5e0!3m2!1ssv!2sse!4v1612862379736!5m2!1ssv!2sse"
+              width="1200"
+              height="1200"
+              frameBorder="0"
+              allowFullScreen=""
+              aria-hidden="false"
+              tabIndex="0"
+            ></iframe>
+          </div>
+          <div className="address_text">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Laudantium asperiores placeat quas, doloremque voluptate ratione
+              sequi optio? Aliquid eligendi voluptatem facere maxime libero,
+              molestias nemo! Officia illo, facere doloribus dicta, sapiente
+              aperiam rem, nam quibusdam amet deserunt cumque sequi impedit
+              consequuntur facilis quisquam debitis quod voluptatem dolorum
+              commodi esse perspiciatis natus aspernatur ipsa. Amet enim placeat
+              distinctio quisquam quaerat quia incidunt eveniet consequuntur,
+              sint, consectetur quas. Mollitia illo incidunt quam modi
+              perspiciatis? Cum quasi consequatur consectetur iste fuga?
+              Voluptatem ipsam animi obcaecati? Sequi ab libero facilis minima
+              nulla iste, aut blanditiis iusto assumenda asperiores at nemo id
+              fuga quam optio.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <BackToTop target="/about/#top" />
     </div>
   );
 };
