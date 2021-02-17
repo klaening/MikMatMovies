625d24c27623d41fd4dfa14e9db3a71f6efaa408
mattisvainikainen <mattis.vainikainen@gmail.com>
2021-02-17 21:04:25 +0100

    added Link smooth to TV-series on serchresult

--- mikmatmovies/src/Pages/Search/SearchResult.js
+++ mikmatmovies/src/Pages/Search/SearchResult.js
@@ -1,22 +1,26 @@
 import React from "react";
 import CardHolder from "../../Components/CardHolder/CardHolder";
+import { HashLink as Link } from "react-router-hash-link";
 
 const SearchResult = ({ SearchResult, seriesSearchResult, searchString }) => {
   return (
     <div>
       <h2>Search results for "{searchString}"</h2>
-
+      <Link smooth to="#TVSERIES">
+        Tv-Series
+      </Link>
       <CardHolder
         header="Movies"
         subTitle={`Results: ${SearchResult.length}`}
         movies={SearchResult}
       />
-
-      <CardHolder
-        header="Series"
-        subTitle={`Results: ${seriesSearchResult.length}`}
-        movies={seriesSearchResult}
-      />
+      <div id="TVSERIES">
+        <CardHolder
+          header="Series"
+          subTitle={`Results: ${seriesSearchResult.length}`}
+          movies={seriesSearchResult}
+        />
+      </div>
     </div>
   );
 };
